ead366e2cc5468a993d657ffa1907b9059142aee
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-04 23:41:04 +0800

    Add files via upload

---
+++ issgraph.py
@@ -0,0 +1,9 @@
+
+from graphics import Graphics
+from sprite import Sprite
+from keyboard import keyPress, bindKeys, keyRelease, keyDown
+from collide import collide, meetPlace
+from color import Color
+
+version = (0, 1, 7)
+codename = "Jammy"
--- sprite.py
+++ sprite.py
@@ -1,9 +1,9 @@
 import turtle
 
 class Sprite():
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+	def __init__(self):
+		self.x = 0
+		self.y = 0
 		self.shape = "square"
 		self.tk = turtle.Turtle()
 		self.tk.penup()
