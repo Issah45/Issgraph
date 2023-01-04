98bedf85494690a0f811851ee1d7c91d59c2f42e
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-04 23:43:56 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-
-from isseng.graphics import Graphics
-from isseng.sprite import Sprite
-from isseng.keyboard import keyPress, bindKeys, keyRelease, keyDown
-from isseng.collide import collide, meetPlace
-from isseng.color import Color
