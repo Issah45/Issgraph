31705502df1cb438809c19a1307d6e56d9f48d3f
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-17 18:29:15 +0800

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Graphics():
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+        self.display = pygame.display.set_mode((w, h))
+        self.KEYDOWN = pygame.KEYDOWN
+        self.KEYUP = pygame.KEYUP
+    def shouldClose(self):
+        return pygame.QUIT
+    def game(self):
+        return pygame.event.get()
+    def paint(self, r, g, b):
+        self.display.fill((r, g, b))
+    def update(self):
+        pygame.display.update()
+    def image(self, img, x, y):
+        self.display.blit(img, (x, y))
+
+def img(i):
+    return pygame.image.load(i)
+
+KEYS = {
+    "right": pygame.K_RIGHT,
+    "left": pygame.K_LEFT,
+    "up": pygame.K_UP,
+    "down": pygame.K_DOWN,
+    "z": pygame.K_z,
+    "x": pygame.K_x,
+    "a": pygame.K_a,
+    "b": pygame.K_b,
+    "esc": pygame.K_ESCAPE,
+    "enter": pygame.K_RETURN,
+    "lshift": pygame.K_LSHIFT,
+    "rshift": pygame.K_RSHIFT
+}
+
+def k(i):
+    n = pygame.key.get_pressed()
+    return n[i]
---
+++ issgraph.py
@@ -0,0 +1,5 @@
+
+from graphics import Graphics, img, KEYS, k
+
+version = (0, 2, 0)
+codename = "Down The Lane"
