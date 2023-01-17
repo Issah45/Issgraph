96aa26f75583fc69f4c547bd7ffad0b2f70570a5
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-17 18:28:33 +0800

    Delete issgraph.py

--- issgraph.py
+++
@@ -1,9 +0,0 @@
-
-from graphics import Graphics
-from sprite import Sprite
-from keyboard import keyPress, bindKeys, keyRelease, keyDown
-from collide import collide, meetPlace
-from color import Color
-
-version = (0, 1, 7)
-codename = "Creamy"
