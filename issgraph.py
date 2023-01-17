69ee219d056eb2e9d10177b8098098acc80f0409
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-18 01:59:41 +0800

    Add files via upload

--- issgraph.py
+++ issgraph.py
@@ -1,7 +1,7 @@
 
 from graphics import Graphics, Img, Utils, Mouse
 
-version = (0, 3, 9)
+version = (0, 3, 10)
 codename = "Spinning Stuff"
 
 print(f"Issgraph Codename: {codename}, Version: {version}")
