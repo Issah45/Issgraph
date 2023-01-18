f68cdf5b65dcb2d829ffc1d6ee1c607e43590a91
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-01-18 14:37:09 +0800

    Add files via upload

--- issgraph.py
+++ issgraph.py
@@ -1,7 +1,7 @@
 
 from graphics import Graphics, Img, Utils, Mouse
 
-version = (0, 3, 14)
-codename = "Running Faster"
+version = (0, 3, 15)
+codename = "High In The Sky"
 
 print(f"Issgraph Codename: {codename}, Version: {version}")
